589b203e687198bb2132f0ca0163235bba0d03f9
majdjb7 <majdjd171@gmail.com>
2021-11-22 03:09:53 +0200

    backend and frontend done

--- dist/APIManager.js
+++ dist/APIManager.js
@@ -1,32 +1,39 @@
 class APIManager {
-    constructor() {
-        this.data = {}
+  constructor() {
+      this.data = {}
+      this.teamToIDs = {
+        "lakers": "1610612747",
+        "warriors": "1610612744",
+        "heat": "1610612748",
+        "suns": "1610612756"
     }
-  
-     generateNBATeam(teamName) {
-        $.ajax({
-          method: "GET",
-          url: 'http://data.nba.net/10s/prod/v1/2018/players.json',
-          success: (response) => {
-            const matchingTeamName = (t) => (t.teamId === teamName)
-            let allPlayers = response.league.standard.filter(matchingTeamName)
+  }
 
-            let getPlayerInfo = allPlayers.map((player) => {
-                return {
-                    firstName: player.firstName,
-                    lastName: player.lastName,
-                    jersey: player.jersey,
-                    position: player.pos
-                }
-            })
-      
+   generateNBATeam(teamName) {
+      let teamNum = this.teamToIDs[teamName]
+      $.ajax({
+        method: "GET",
+        url: 'http://data.nba.net/10s/prod/v1/2018/players.json',
+        success: (response) => {
+          const matchingTeamName = (t) => (t.teamId === teamNum)
+          let allPlayers = response.league.standard.filter(matchingTeamName)
+
+          let getPlayerInfo = allPlayers.map((player) => {
+              return {
+                  firstName: player.firstName,
+                  lastName: player.lastName,
+                  jersey: player.jersey,
+                  position: player.pos
+              }
+          })
     
-            this.data.allPlayers = getPlayerInfo.splice(0, 6);
-            console.log(this.data.allPlayers)
-          },
-          error: function (xhr, text, error) {
-            console.log(text);
-          },
-        });
-      }
+  
+          this.data.allPlayers = getPlayerInfo.splice(0, 6);
+          console.log(this.data.allPlayers)
+        },
+        error: function (xhr, text, error) {
+          console.log(text);
+        },
+      });
+    }
 }
--- server.js
+++ server.js
@@ -1,63 +1,44 @@
-// import APIManager from "./dist/APIManager.js"
+// const APIManager = require('./dist/APIManager')
 
 const express = require('express')
 const path = require('path')
+const urllib = require('urllib')
+const teamData = require('./dist/data')
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
-// const apiManager = new APIManager();
 let port = 3000
 
-
-const teamToIDs = {
-    "lakers": "1610612747",
-    "warriors": "1610612744",
-    "heat": "1610612748",
-    "suns": "1610612756"
-}
-
 app.get('/', function(request, response) {
-    // apiManager.generateNBATeam()
-
-
-
     let priceObj = {price: 500}
     response.send(priceObj)
 })
 
-//Exercise 4
-// app.get('/buy/:name', function(request, response) {
-//     let name = request.params.name
-//     for(let obj of store) {
-//         if(obj.name === name) {
-//             obj.inventory -= 1
-//             response.send(`Congrats! You've just bought ${obj.name} for ${obj.price}. There are ${obj.inventory} left in stock.`)
-//         }
-//     }
+app.get('/teams/:teamName', function (request, response) {
+    const myTeam = request.params.teamName
+    const myTeamId = teamData.teamToIDs[myTeam]
 
+    urllib.request('http://data.nba.net/10s/prod/v1/2018/players.json', function (err, data, res) {
+        const teamData = data.toString()
+        const jsonData = JSON.parse(teamData)
 
-//     response.end()
-// })
+        const matchingTeamName = (t) => ( t.teamId === myTeamId && t.isActive == true)
+        let allPlayers = jsonData.league.standard.filter(matchingTeamName)
 
-// app.get('/sale', function(request, response) {
-//     let q = request.query
-//     if(q.admin) {
-//         for(let obj of store) {
-//             if(obj.inventory > 10){
-//                 obj.price *= 0.5
-
-
-//             }
-//             console.log(obj.price)
-//         }
-//     }
-
-
-//       console.log(users)
-//     response.end()
-// })
+        const finalTeamInfo = allPlayers.map((player) => {
+            return {
+                firstName: player.firstName,
+                lastName: player.lastName,
+                jersey: player.jersey,
+                position: player.pos
+            }
+        })
+        response.send(finalTeamInfo)
+    });
+    
+})
 
 
 
